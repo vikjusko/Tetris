5ae7f345abb69ef2aabc26d1b9bca2c446fd7926
Victoria Jusko <vikjusko@gmail.com>
2021-01-18 14:39:15 +0000

    Shapes move down and freeze

--- app.js
+++ app.js
@@ -70,5 +70,19 @@ document.addEventListener('DOMContentLoaded', () => {
         undraw()
         currentPosition += width
         draw()
+        freeze()
     }
+
+    function freeze() {
+        if (current.some(index => squares[currentPosition + index + width].classList.contains('taken'))) {
+            current.forEach(index => squares[currentPosition + index].classList.add('taken'))
+            random = Math.floor(Math.random() * theShapes.length)
+            current = theShapes[random][currentRotation]
+            currentPosition = 4
+            draw()
+        }
+
+    }
+
+
 })
